3396dd4abd591f62398014f2b96ec1ab9f8b1e91
Heather Johnson-Ledlie <heatherjohnson120@yahoo.com>
2019-05-16 14:19:55 -0400

    finished weather api

--- public/main.js
+++ public/main.js
@@ -1,7 +1,23 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+const getWeatherFromAPI = () => {
+  document.querySelector('.temp').textContent = ' '
+  const input = document.querySelector('.search-bar').value
+  fetch(
+    'https://api.openweathermap.org/data/2.5/weather?q=' +
+      input +
+      '&appid=5f1939fad7de2144c849eee2d5be2d56' +
+      '&units=imperial'
+  )
+    .then(resp => {
+      console.log(resp)
+      return resp.json()
+    })
+    .then(weatherData => {
+      console.log(weatherData)
+      // update the HTML
+      document.querySelector('.temp').textContent = weatherData.main.temp
+      document.querySelector('.sky').textContent +=
+        weatherData.weather[0].description
+    })
 }
 
-document.addEventListener('DOMContentLoaded', main)
+document.querySelector('button').addEventListener('click', getWeatherFromAPI)
